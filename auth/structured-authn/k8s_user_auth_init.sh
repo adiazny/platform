5f783c94b4076cbc0245b5299ea69083c4e940ae
Alan Diaz <adiazny@gmail.com>
2024-12-13 06:56:00 -0500

    structured auth config kind setup

--- auth/structured-authn/k8s_user_auth_init.sh
+++ auth/structured-authn/k8s_user_auth_init.sh
@@ -11,6 +11,8 @@ mint_id_token() {
   password=$3
   client_id=$4
   client_secret=$5
+  audience=$6
+  scope=$7
 
   response=$(curl -s --request POST \
     --url ${oauth_token_url} \
@@ -18,9 +20,10 @@ mint_id_token() {
     --data grant_type=password \
     --data "username=${username}" \
     --data "password=${password}" \
-    --data scope='openid email' \
+    --data scope="${scope}" \
     --data "client_id=${client_id}" \
-    --data "client_secret=${client_secret}")
+    --data "client_secret=${client_secret}" \
+    --data "audience=${audience}")
 
   if [ $? -ne 0 ]; then
     handle_error "Failed to mint OIDC id_token"
@@ -64,32 +67,79 @@ authorize_user() {
 
 echo -e "Minting OIDC id_token by way of the OAUTH2 Resource Owner Password Grant type.\n"
 
-# AMAPIANO USER
-oauth_token_url="https://amapiano-idp.us.auth0.com/oauth/token"
-idp_issuer_url="https://amapiano-idp.us.auth0.com/"
-username="demo@runtime.diaz"
-password="Demo12345"
-client_id="jLe7RD4MqaW6fFgRwOXa8B0n3OVFt4Z7"
-client_secret="BCSXWL1IrhHz2WgqT1FLGpAxa6QNRL-6He5IJgSLxP65uV1PAcccjx_5knRGFTLH"
+add_user_to_kubeconfig() {
+  oauth_token_url=$1
+  idp_issuer_url=$2
+  username=$3
+  password=$4
+  client_id=$5
+  client_secret=$6
+  audience=$7
 
-id_token=$(mint_id_token "${oauth_token_url}" "${username}" "${password}" "${client_id}" "${client_secret}")
+  id_token=$(
+    mint_id_token \
+      "${oauth_token_url}" \
+      "${username}" \
+      "${password}" \
+      "${client_id}" \
+      "${client_secret}" \
+      "${audience}"
+  )
 
-echo "${id_token}"
+  echo -e "${id_token}\n"
 
-if [ -z "${id_token}" ]; then
-  handle_error "id_token is empty. Check your Auth0 configuration."
-fi
+  if [ -z "${id_token}" ]; then
+    handle_error "id_token is empty. Check your Auth0 configuration."
+  fi
 
-echo -e "Adding the id_token and authentication provider configuration to kubectl's kubeconfig.\n"
+  echo -e "Adding the id_token and authentication provider configuration to kubectl's kubeconfig.\n"
 
-configure_kubectl "${username}" "${idp_issuer_url}" "${id_token}" "${client_id}"
+  configure_kubectl "${username}" "${idp_issuer_url}" "${id_token}" "${client_id}"
 
-echo -e "\nAuthorizing the demo user by binding RBAC.\n"
+  echo -e "\nAuthorizing the demo user by binding RBAC.\n"
 
-authorize_user "${username}"
+  authorize_user "${username}"
 
-echo -e "\nValidate you have access to the cluster API by running the following commands.\n"
-echo "kubectl --user=${username} auth whoami"
-echo "kubectl --user=${username} get nodes"
+  echo -e "\nValidate you have access to the cluster API by running the following commands.\n"
+  echo "kubectl --user=${username} auth whoami"
+  echo "kubectl --user=${username} get nodes"
+}
+
+
+# AMAPIANO IDP USER
+amapiano_oauth_token_url="https://amapiano-idp.us.auth0.com/oauth/token"
+amapiano_idp_issuer_url="https://amapiano-idp.us.auth0.com/"
+amapiano_username="jonny@runtime.diaz"
+amapiano_password="Demo12345"
+amapiano_client_id="jLe7RD4MqaW6fFgRwOXa8B0n3OVFt4Z7"
+amapiano_client_secret="BCSXWL1IrhHz2WgqT1FLGpAxa6QNRL-6He5IJgSLxP65uV1PAcccjx_5knRGFTLH"
+amapiano_audience="https://amapiano-idp.us.auth0.com/api/v2/"
+
+add_user_to_kubeconfig \
+  "${amapiano_oauth_token_url}" \
+  "${amapiano_idp_issuer_url}" \
+  "${amapiano_username}" \
+  "${amapiano_password}" \
+  "${amapiano_client_id}" \
+  "${amapiano_client_secret}" \
+  "${amapiano_audience}"
+
+# SALSA IDP USER
+salsa_oauth_token_url="https://salsa-idp.us.auth0.com/oauth/token"
+salsa_idp_issuer_url="https://salsa-idp.us.auth0.com/"
+salsa_username="foo@salsa.dev"
+salsa_password="Foo12345"
+salsa_client_id="u2vEGqwWnlf2QtXu56dhWOCwydMtY2n5"
+salsa_client_secret="_l4GmT25-rV21wNoD_gcxXgm42C1ZWbtvZXUykIjoEyNypUeCIq9B2pTzHwUy5no"
+salsa_audience="https://salsa-idp.us.auth0.com/api/v2/"
+
+add_user_to_kubeconfig \
+  "${salsa_oauth_token_url}" \
+  "${salsa_idp_issuer_url}" \
+  "${salsa_username}" \
+  "${salsa_password}" \
+  "${salsa_client_id}" \
+  "${salsa_client_secret}" \
+  "${salsa_audience}"
 
 chmod +x k8s_user_auth_init.sh
