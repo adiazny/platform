5495a12aef60c5c1e94c30d0f504dce4c930875d
Alan Diaz <adiazny@gmail.com>
2024-12-13 07:57:33 -0500

    Update bash script to pass in variables from cli

--- auth/structured-authn/k8s_user_auth_init.sh
+++ auth/structured-authn/k8s_user_auth_init.sh
@@ -11,8 +11,7 @@ mint_id_token() {
   password=$3
   client_id=$4
   client_secret=$5
-  audience=$6
-  scope=$7
+  scope=$6
 
   response=$(curl -s --request POST \
     --url ${oauth_token_url} \
@@ -22,11 +21,14 @@ mint_id_token() {
     --data "password=${password}" \
     --data scope="${scope}" \
     --data "client_id=${client_id}" \
-    --data "client_secret=${client_secret}" \
-    --data "audience=${audience}")
+    --data "client_secret=${client_secret}")
 
   if [ $? -ne 0 ]; then
     handle_error "Failed to mint OIDC id_token"
+  fi
+
+  if echo "${response}" | grep -i "error"; then
+    handle_error "${response}"
   fi
 
   id_token=$(echo "${response}" | jq -r .id_token)
@@ -58,14 +60,19 @@ authorize_user() {
   username=$1
   user=${username/%@*/}
 
-  kubectl create clusterrolebinding ${user}-oidc-cluster-admin --clusterrole=cluster-admin --user="${username}"
+  if kubectl get clusterrolebinding | grep -q "${user}"-oidc-cluster-admin; then
+    echo -e "\nDeleting previous clusterrolebinding for user ${user}"
+    kubectl delete clusterrolebinding "${user}"-oidc-cluster-admin
+  fi
+
+  group=$(kubectl --user="${username}" auth whoami -o json | jq -r '.status.userInfo.groups[] | select(startswith("system") | not)')
+
+  kubectl create clusterrolebinding "${user}"-oidc-cluster-admin --clusterrole=cluster-admin --group="${group}"
 
   if [ $? -ne 0 ]; then
-    handle_error "Failed to authorize user "${email}" with RBAC"
+    handle_error "Failed to authorize user "${username}" with RBAC"
   fi
 }
-
-echo -e "Minting OIDC id_token by way of the OAUTH2 Resource Owner Password Grant type.\n"
 
 add_user_to_kubeconfig() {
   oauth_token_url=$1
@@ -74,7 +81,7 @@ add_user_to_kubeconfig() {
   password=$4
   client_id=$5
   client_secret=$6
-  audience=$7
+  scope=$7
 
   id_token=$(
     mint_id_token \
@@ -83,10 +90,10 @@ add_user_to_kubeconfig() {
       "${password}" \
       "${client_id}" \
       "${client_secret}" \
-      "${audience}"
+      "${scope}"
   )
 
-  echo -e "${id_token}\n"
+  echo -e "id_token: ${id_token}\n"
 
   if [ -z "${id_token}" ]; then
     handle_error "id_token is empty. Check your Auth0 configuration."
@@ -105,41 +112,53 @@ add_user_to_kubeconfig() {
   echo "kubectl --user=${username} get nodes"
 }
 
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --oauth-token-url)
+      OAUTH_TOKEN_URL="$2"
+      shift 2
+      ;;
+    --idp-issuer-url)
+      IDP_ISSUER_URL="$2"
+      shift 2
+      ;;
+    --username)
+      USERNAME="$2"
+      shift 2
+      ;;
+    --password)
+      PASSWORD="$2"
+      shift 2
+      ;;
+    --client-id)
+      CLIENT_ID="$2"
+      shift 2
+      ;;
+    --client-secret)
+      CLIENT_SECRET="$2"
+      shift 2
+      ;;
+    --scope)
+      SCOPE="$2"
+      shift 2
+      ;;
+    *)
+      echo "Unknown flag: $1"
+      exit 1
+      ;;
+  esac
+done
 
-# AMAPIANO IDP USER
-amapiano_oauth_token_url="https://amapiano-idp.us.auth0.com/oauth/token"
-amapiano_idp_issuer_url="https://amapiano-idp.us.auth0.com/"
-amapiano_username="jonny@runtime.diaz"
-amapiano_password="Demo12345"
-amapiano_client_id="jLe7RD4MqaW6fFgRwOXa8B0n3OVFt4Z7"
-amapiano_client_secret="BCSXWL1IrhHz2WgqT1FLGpAxa6QNRL-6He5IJgSLxP65uV1PAcccjx_5knRGFTLH"
-amapiano_audience="https://amapiano-idp.us.auth0.com/api/v2/"
+if [ -z "${OAUTH_TOKEN_URL}" ] || [ -z "${IDP_ISSUER_URL}" ] || [ -z "${USERNAME}" ] || [ -z "${PASSWORD}" ] || [ -z "${CLIENT_ID}" ] || [ -z "${CLIENT_SECRET}" ] || [ -z "${SCOPE}" ]; then
+  echo "All flags must be provided"
+  exit 1
+fi
 
 add_user_to_kubeconfig \
-  "${amapiano_oauth_token_url}" \
-  "${amapiano_idp_issuer_url}" \
-  "${amapiano_username}" \
-  "${amapiano_password}" \
-  "${amapiano_client_id}" \
-  "${amapiano_client_secret}" \
-  "${amapiano_audience}"
-
-# SALSA IDP USER
-salsa_oauth_token_url="https://salsa-idp.us.auth0.com/oauth/token"
-salsa_idp_issuer_url="https://salsa-idp.us.auth0.com/"
-salsa_username="foo@salsa.dev"
-salsa_password="Foo12345"
-salsa_client_id="u2vEGqwWnlf2QtXu56dhWOCwydMtY2n5"
-salsa_client_secret="_l4GmT25-rV21wNoD_gcxXgm42C1ZWbtvZXUykIjoEyNypUeCIq9B2pTzHwUy5no"
-salsa_audience="https://salsa-idp.us.auth0.com/api/v2/"
-
-add_user_to_kubeconfig \
-  "${salsa_oauth_token_url}" \
-  "${salsa_idp_issuer_url}" \
-  "${salsa_username}" \
-  "${salsa_password}" \
-  "${salsa_client_id}" \
-  "${salsa_client_secret}" \
-  "${salsa_audience}"
-
-chmod +x k8s_user_auth_init.sh
+  "${OAUTH_TOKEN_URL}" \
+  "${IDP_ISSUER_URL}" \
+  "${USERNAME}" \
+  "${PASSWORD}" \
+  "${CLIENT_ID}" \
+  "${CLIENT_SECRET}" \
+  "${SCOPE:-"openid email"}"
